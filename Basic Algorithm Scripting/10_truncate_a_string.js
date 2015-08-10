1abe7d13379820f7c89ef333673883bfc6d728ee
Tien Ta <taminhtien1993@gmail.com>
2015-08-10 16:26:56 +0700

    Add bonfire truncate a string

---
+++ Basic Algorithm Scripting/10_truncate_a_string.js
@@ -0,0 +1,20 @@
+/*
+	Truncate a string (first argument) if it is longer than the given maximum string length 
+	(second argument). Return the truncated string with a '...' ending.
+	Note that the three dots at the end add to the string length.
+*/
+
+function truncate(str, num) {
+  // Clear out that junk in your trunk
+  if(str.length > num) {
+    while (str[num] != ' ') {
+      str = str.slice(0, num);
+      num--;
+    }
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
+truncate('A-tisket a-tasket A green and yellow basket', 11);
