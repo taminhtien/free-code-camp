c7f7a0e4b3bf18bfb1953e1ed38de04b9eec004d
Tien Ta <taminhtien1993@gmail.com>
2015-08-14 16:34:59 +0700

    Add bonfire checking boolean primitive

---
+++ Basic Algorithm Scripting/25_boo_who.js
@@ -0,0 +1,20 @@
+/*
+	Check if a value is classified as a boolean primitive. Return true or false.
+	Boolean primitives are true and false.
+*/
+
+function boo(bool) {
+  if(bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+boo(null);
+
+// Another way
+// Author: Kirill Morozov
+function boo(bool) {
+  return typeof bool === 'boolean';
+}
