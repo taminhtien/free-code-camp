4347e725340a7c7dcab69d4b5e34d51832b87dfd
Tien Ta <taminhtien1993@gmail.com>
2015-08-17 17:54:45 +0700

    Add bonfire computing sum of odd fibonacci number

---
+++ Basic Algorithm Scripting/29_sum_all_odd_fibo.js
@@ -0,0 +1,24 @@
+/*
+  Return sum of all odd fibonacci numbers up to and including the passed number
+  if it is a Fibonacci number
+*/
+
+function fibo(num) {
+  if(num <= 1) {
+    return 1;
+  }
+  return fibo(num - 1) + fibo(num - 2);
+}
+
+function sumFibs(num) {
+  var sum = 0;
+  for(i = 0; i <= num; i++) {
+    var f = fibo(i);
+    if(f % 2 !== 0) {
+      sum += f;
+    }
+  }
+  return sum;
+}
+
+sumFibs(4);
